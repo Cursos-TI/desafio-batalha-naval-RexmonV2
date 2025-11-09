2e9e064ec24b37c8d63d23fbb797f7f3bac4f1ff
Felipe Machado <felipe_machado40@hotmail.com>
2025-11-09 18:31:53 +0000

    Nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,70 +1,54 @@
 #include <stdio.h> 
 
-//Definindo as contantes 
-#define TAMANHO 10    
-#define TAM_NAVIO 3   
+//Definindo as constantes
+#define TAMANHO 10
+#define TAM_NAVIO 3
 #define AGUA 0
 #define NAVIO 3
 
 int main() {
     
-    //Representa o Tabuleiro
+    //Criando o tabuleiro
     int tabuleiro[TAMANHO][TAMANHO];
-    
-    // para o controle dos loops
-    int i, j;
+    int i, j; 
 
-    //Começando o o tabuleiro com (0)
     for (i = 0; i < TAMANHO; i++) {
         for (j = 0; j < TAMANHO; j++) {
             tabuleiro[i][j] = AGUA;
         }
     }
 
-    //Declara os navios como vetores
-    int navio_modelo[TAM_NAVIO] = {NAVIO, NAVIO, NAVIO};
+    //Posicionaer os 4 navios 
 
-
-    //Define as coordenadas iniciais 
-    
-    //Navio Horizontal começa na Linha 1, Coluna 2
-    int h_linha = 2;
-    int h_col_inicio = 1;
-    
-    //Navio Vertical começa na Linha 4, Coluna 5
-    int v_linha_inicio = 4;
-    int v_col = 5;
-
-    
-    //Posiciona os Navios no Tabuleiro
-    
-    // Posiciona Navio Horizontal
-    printf("Posicionando Navio Horizontal...\n");
+    //Navio Horizontal Linha 1 e Coluna 1
     for (i = 0; i < TAM_NAVIO; i++) {
-        tabuleiro[h_linha][h_col_inicio + i] = navio_modelo[i];
+        tabuleiro[1][1 + i] = NAVIO;
     }
 
-    // Posiciona Navio Vertical
-    printf("Posicionando Navio Vertical...\n");
+    //Navio Vertical Linha 3 e Coluna 8
     for (i = 0; i < TAM_NAVIO; i++) {
-        tabuleiro[v_linha_inicio + i][v_col] = navio_modelo[i];
+        tabuleiro[3 + i][8] = NAVIO;
+    }
+
+    //Navio Diagonal Linha 4 e Coluna 2
+    for (i = 0; i < TAM_NAVIO; i++) {
+        tabuleiro[4 + i][2 + i] = NAVIO;
+    }
+
+    //Navio Diagonal Linha 7 e Coluna 3
+    for (i = 0; i < TAM_NAVIO; i++) {
+        tabuleiro[7 + i][3 - i] = NAVIO;
     }
 
 
-    //Exibe o Tabuleiro
-    printf("\n=== Tabuleiro de Batalha Naval ===\n");
-    
-    // Loop das linhas
+    //monstrando o tabuleiro
+    printf("=== Tabuleiro Aventureiro (Simples) ===\n");
     for (i = 0; i < TAMANHO; i++) {
-        
-        // Loop das colunas
         for (j = 0; j < TAMANHO; j++) {
-            // mostra o valor da célula (0 ou 3) 
             printf("%d ", tabuleiro[i][j]);
         }
-        
         printf("\n"); 
     }
 
-    return 0; 
+    return 0;
 }
